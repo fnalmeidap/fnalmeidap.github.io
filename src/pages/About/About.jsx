1bf333bec107e2b25c9882a174e159416994496d
Felipe Nunes <fnap@cin.ufpe.br>
2021-05-29 17:35:55 -0300

    new about section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import "../../assets/css/about.css";
-import computer from "../../assets/img/computador.png"
-import memphis from "../../assets/img/memphis.png"
-import arcade from "../../assets/img/maquina-de-arcade.png"
-import math from "../../assets/img/square-root.png"
-import network from "../../assets/img/browser.png"
-import phyandeletro from "../../assets/img/electronic-circuit.png"
+import computer from "../../assets/img/computador.png";
+import memphis from "../../assets/img/memphis.png";
+import arcade from "../../assets/img/maquina-de-arcade.png";
+import math from "../../assets/img/square-root.png";
+import network from "../../assets/img/browser.png";
+import phyandeletro from "../../assets/img/electronic-circuit.png";
 
 import me from "../../assets/img/me.jpeg";
 
@@ -47,11 +47,13 @@ class About extends Component {
         </nav>
         <nav className="about-middle">
           <div class="container">
-            <h1 className="about-title" id="main-middle-title">What am i interested in?</h1>
+            <h1 className="about-title" id="main-middle-title">
+              What am i interested in?
+            </h1>
             <div class="row">
               <div class="col middle-col">
-              <img clasname="about-middle_image" src={computer}></img>
-              <h3 id="middle-title">Python and C++ programming</h3>
+                <img clasname="about-middle_image" src={computer}></img>
+                <h3 id="middle-title">Python and C++ programming</h3>
               </div>
               <div class="col middle-col">
                 <img clasname="about-middle_image" src={memphis}></img>
@@ -64,8 +66,8 @@ class About extends Component {
             </div>
             <div class="row">
               <div class="col middle-col">
-              <img clasname="about-middle_image" src={math}></img>
-              <h3 id="middle-title">Mathematics</h3>
+                <img clasname="about-middle_image" src={math}></img>
+                <h3 id="middle-title">Mathematics</h3>
               </div>
               <div class="col middle-col">
                 <img clasname="about-middle_image" src={phyandeletro}></img>
@@ -76,8 +78,105 @@ class About extends Component {
                 <h3 id="middle-title">Network Infrastructure</h3>
               </div>
             </div>
-            
           </div>
+        </nav>
+        <nav className="about-end">
+          <div className="container">
+            <div className="about-end-description">
+              <h1 className="main-title">What have i already done?</h1>
+            </div>
+
+            <div class="accordion" id="accordionExample">
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                  <button
+                    class="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseOne"
+                    aria-expanded="true"
+                    aria-controls="collapseOne"
+                  >
+                    Scientific article publication and Symposium presentation
+                  </button>
+                </h2>
+                <div
+                  id="collapseOne"
+                  class="accordion-collapse collapse show"
+                  aria-labelledby="headingOne"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div class="accordion-body">
+
+                    The process of creating a way to analyze 2D Soccer Simulation
+                    games was so good and seemingly new among the competition teams
+                    that we decided to write an article about it and submit to the <b>Latin American
+                    Robotics Symposium LARS</b> at the <b>8th Brazilian Symposium of Robotics</b>. 
+                    We got accepted and i virtually presented what we did in the article. 
+                    This also granted us a publication in the <b>IEEXplore</b>. Link
+                     is <a target="blank" href="https://ieeexplore.ieee.org/document/9307104">here</a>.
+                  </div>
+                </div>
+              </div>
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                  <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseTwo"
+                    aria-expanded="false"
+                    aria-controls="collapseTwo"
+                  >
+                    PET Informática hackatoon
+                  </button>
+                </h2>
+                <div
+                  id="collapseTwo"
+                  class="accordion-collapse collapse"
+                  aria-labelledby="headingTwo"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div class="accordion-body">
+                      In July 2019 i had my first experience coding a real application from scratch in
+                      a hackatoon presented by our centre's PET team. There i had a one week bootcamp 
+                      about web technologies that we would use in the event such as <b>Javascript, HTML, CSS,
+                      React</b> and <b>Node.js</b>.
+                  </div>
+                </div>
+              </div>
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                  <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseThree"
+                    aria-expanded="false"
+                    aria-controls="collapseThree"
+                  >
+                    2D Simulation log analyzer
+                  </button>
+                </h2>
+                <div
+                  id="collapseThree"
+                  class="accordion-collapse collapse"
+                  aria-labelledby="headingThree"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div class="accordion-body">
+                    This is my best project so far! Here i had to design
+                    and develop a framework to analyze through a heavy load
+                    of data from the RobôCIn team robots and ML Agents.
+                    To do this i used technlogies such as <b>Linux, Python, Pandas, Docker </b> 
+                     and <b>React</b>.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <nav className="about-know-more">
         </nav>
       </section>
     );
