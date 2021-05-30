68bdf6fe2e9e2f95779be0b4cddf0c86b0b4b177
Felipe Nunes <fnap@cin.ufpe.br>
2021-05-30 17:50:09 -0300

    style(Contact): new page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,14 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "../../assets/css/contact.css";
+import git from "../../assets/img/github.png";
+import insta from "../../assets/img/instagram.png";
+import linkedin from "../../assets/img/linkedin.png";
 
-class Contact extends Component{
-  render(){
+class Contact extends Component {
+  render() {
     return (
       <section className="contact">
-        <h1>Under construction.</h1>
+        <nav className="contact-banner">
+          <div className="container">
+            <h1>Get in touch!</h1>
+          </div>
+          <div class="container contact-links">
+            <div class="row">
+              <div class="col">
+                <img className="contact-image" src={git}></img>
+                <a class="btn btn-primary" target="blank" href="https://github.com/fnalmeidap" role="button">
+                  Github
+                </a>
+              </div>
+              <div class="col">
+              <img className="contact-image" src={linkedin}></img>
+                <a class="btn btn-primary" target="blank" href="https://www.linkedin.com/in/fnap/" role="button">
+                  LinkedIn
+                </a>
+              </div>
+              <div class="col">
+              <img className="contact-image" src={insta}></img>
+                <a class="btn btn-primary" target="blank" href="https://www.instagram.com/f_nap/" role="button">
+                  Instagram
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
       </section>
     );
   }
-} 
+}
 
 export default Contact;
