8113b342444308e4dd6c9c486279adfca2a97a55
Felipe Nunes <fnap@cin.ufpe.br>
2021-05-30 18:39:59 -0300

    style(About): adapted to mobile version

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -9,33 +9,45 @@ class Contact extends Component {
     return (
       <section className="contact">
         <nav className="contact-banner">
-          <div className="container">
-            <h1>Get in touch!</h1>
-          </div>
-          <div class="container contact-links">
-            <div class="row">
-              <div class="col">
-                <img className="contact-image" src={git}></img>
-                <a class="btn btn-primary" target="blank" href="https://github.com/fnalmeidap" role="button">
-                  Github
-                </a>
-              </div>
-              <div class="col">
+          <h1>Get in touch!</h1>
+          <div class="contact-links">
+            <div class="link-item">
+              <img className="contact-image" src={git}></img>
+              <a
+                class="btn btn-primary"
+                target="blank"
+                href="https://github.com/fnalmeidap"
+                role="button"
+              >
+                Github
+              </a>
+            </div>
+            <div class="link-item">
               <img className="contact-image" src={linkedin}></img>
-                <a class="btn btn-primary" target="blank" href="https://www.linkedin.com/in/fnap/" role="button">
-                  LinkedIn
-                </a>
-              </div>
-              <div class="col">
+              <a
+                class="btn btn-primary"
+                target="blank"
+                href="https://www.linkedin.com/in/fnap/"
+                role="button"
+              >
+                LinkedIn
+              </a>
+            </div>
+            <div class="link-item">
               <img className="contact-image" src={insta}></img>
-                <a class="btn btn-primary" target="blank" href="https://www.instagram.com/f_nap/" role="button">
-                  Instagram
-                </a>
-              </div>
+              <a
+                class="btn btn-primary"
+                target="blank"
+                href="https://www.instagram.com/f_nap/"
+                role="button"
+              >
+                Instagram
+              </a>
             </div>
           </div>
           <div className="contact-email">
-            <h1>Email me through: <b id="ht">fnap@cin.ufpe.br</b></h1>
+            <h2>Email me</h2>
+            <h2 id="fnap"><b id="ht">fnap@cin.ufpe.br</b></h2>
           </div>
         </nav>
       </section>
